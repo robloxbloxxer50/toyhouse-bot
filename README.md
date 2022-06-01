0ac696a6b64beebfbb796309720a57dad69b9e0b
Bloxxer <70298893+robloxbloxxer50@users.noreply.github.com>
2022-06-01 12:36:01 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# roleplay-bot
+A bot that allows you to create character descriptions and view them at any time.
+
+This bots purpose is basically to have an easier way of viewing a characters info, nothing more really.
+
+This bot was made entirely by Bloxxer#8729 on Discord using Node.js
---
+++ bot-dev.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const slasho = require("discord-slasho")
+const { CommandInteraction } = require("discord.js")
+
+//Command Handler Script
+const cmds = require("./cmds/handler.js")
+
+
+const bot = new slasho.App({
+    token: process.env.DISCORD_TOKEN_DEV,
+    devGuild: "714337883116535868",
+    intents: ["GUILDS"],
+    commands: [
+        cmds.makechar,
+        cmds.editchar,
+        cmds.deletechar,
+        cmds.viewchar,
+        cmds.listchars,
+        cmds.info,
+        //cmds.test
+    ]
+})
+
+bot.launch().then(() => {
+    bot.dev()
+})
